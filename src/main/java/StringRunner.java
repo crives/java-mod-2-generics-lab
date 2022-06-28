2a2833207055c9dd40fff3c9beb3d37fb74e1521
Caroline Rives <car.rives@gmail.com>
2022-06-28 15:05:04 -0400

    Generics Lab Complete

--- src/main/java/StringRunner.java
+++ src/main/java/StringRunner.java
@@ -1,5 +1,9 @@
+import java.util.Arrays;
+
 class Problem {
-  // declare the convertToString method here
+  static <E> String convertToString(E[] array) {
+    return Arrays.toString(array);
+  }
 }
 
 public class StringRunner {
@@ -7,8 +11,8 @@ public class StringRunner {
     Integer[] inputInt = { 1, 2, 3 };
     String[] inputStr = { "Cake", "Donut", "Cupcake" };
 
-    // System.out.println(Problem.convertToString(inputInt)); // [1, 2, 3]
-    // System.out.println(Problem.convertToString(inputStr)); // [Cake, Donut,
+    System.out.println(Problem.convertToString(inputInt)); // [1, 2, 3]
+    System.out.println(Problem.convertToString(inputStr)); // [Cake, Donut,
     // Cupcake]
   }
 }
